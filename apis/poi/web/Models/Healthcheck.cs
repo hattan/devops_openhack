034b8c8ee039c99b7d2df792807af399a2314771
Hattan Shobokshi <hacker2muk@OTAProd52ops.onmicrosoft.com>
2019-11-26 19:15:03 +0000

    adding missing semi-colon. I've been doing go for too long.

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -9,7 +9,7 @@ namespace poi.Models
         {
             Message = "POI Service Healthcheck";
             Status = "Healthy";
-            Version = "hattan-v1"
+            Version = "hattan-v1";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
